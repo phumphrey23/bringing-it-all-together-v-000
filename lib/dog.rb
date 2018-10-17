e30d881cd02f4d293dd2f0de36595689bf7ac42e
Precious Humphrey <phumphrey23@gmail.com>
2018-10-17 15:47:36 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -45,5 +45,31 @@ class Dog
       dog
     end
 
+    def self.new_from_db(row)
+      id = row[0]
+      name = row[1]
+      breed = row[2]
+      self.new(id, name, breed)
+    end
+
+    def self.find_by_name(name)
+      sql = <<-SQL
+        SELECT *
+        FROM dogs
+        WHERE name = ?
+        LIMIT 1
+      SQL
+
+      DB[:conn].execute(sql, name).map do |row|
+        self.new_from_db(row)
+      end.first
+    end
+
+    def update
+      sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+      DB[:conn].execute(sql, self.name, self.breed, self.id)
+    end
+
+
 
 end
